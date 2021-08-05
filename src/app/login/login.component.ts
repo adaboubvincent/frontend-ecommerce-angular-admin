134b8340bd4ee69cb58646722004463e1e00ac1b
adaboubvincent <adaboubvincent@gmail.com>
2021-08-05 01:29:35 +0000

    Update ts and model user...

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -36,17 +36,21 @@ export class LoginComponent implements OnInit {
     this.securityService.login(user).subscribe((res) => {
       let dict = res;
       console.log(res);
-      if(res.user?.is_superuser !== true){
+      if(res.user?.role != 'ADMIN'){
         this.securityService.notificationAjouter("Vous ne pouvez pas se connecter sur ce site.\nVous devez avoir un accès Admin", "warning");
       }else{
-        console.log(dict);
-        localStorage.setItem('token', res.token || "");
-        localStorage.setItem('username', res.user?.username || "");
-        localStorage.setItem('email', res.user?.email || "");
-        localStorage.setItem('is_superuser', ""+res.user?.is_superuser || "");
-        localStorage.setItem('pageReload', 'true');
-        this.securityService.notificationAjouter("Vous êtes connecté maintenant!", "success");
-        
+        if(res.user?.is_active == true){
+          localStorage.setItem('token', res.token || "");
+          localStorage.setItem('username', res.user?.username || "");
+          localStorage.setItem('email', res.user?.email || "");
+          localStorage.setItem('is_superuser', ""+res.user?.is_superuser || "");
+          localStorage.setItem('role', ""+res.user?.role || "");
+          localStorage.setItem('pageReload', 'true');
+          this.securityService.notificationAjouter("Vous êtes connecté maintenant!", "success");
+        }else{
+          this.securityService.notificationAjouter(res?.test || "" , "warning");
+        }
+
         this.route.navigateByUrl('');
         //window.location.reload();
       }
