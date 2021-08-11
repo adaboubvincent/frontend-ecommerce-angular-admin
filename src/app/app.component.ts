efdf721193150f645235f184314bb22655f60830
renaud-kondo <renaudkondo2@gmail.com>
2021-08-11 14:11:24 +0000

    separated login component for more efficiency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {Title} from "@angular/platform-browser";
+import {SecurityService} from "./services/user/security.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,8 +10,31 @@ import {Title} from "@angular/platform-browser";
 })
 export class AppComponent {
   title = 'frontendEcommerceAdmin';
-  public constructor(private titleService: Title) {  }
+
+  username: string | null = "";
+  is_superuser: string | null = "";
+  role: string | null = "";
+
+  public constructor(private titleService: Title,private route: Router, private securityService: SecurityService) {
+    this.securityService.user(localStorage.getItem('token')).subscribe((res) => {
+        let r = res;
+      },
+      (error) => {
+        localStorage.setItem('token', "");
+        localStorage.setItem('id', "");
+        localStorage.setItem('username', "");
+        localStorage.setItem('email', "");
+        localStorage.setItem('is_superuser', "");
+        localStorage.setItem('role', "");
+      });
+
+    this.username = localStorage.getItem('username');
+    this.is_superuser = localStorage.getItem('is_superuser');
+    this.role = localStorage.getItem('role');
+  }
   ngOnInit(): void {
     this.titleService.setTitle('MAYAKI');
   }
+
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ import { Router } from '@angular/router';
   '../../assets/css/bootstrap.min.css']
 })
 export class LoginComponent implements OnInit {
-  
+
   loadingPage: boolean = false;
 
   formLogin = this.fb.group({
@@ -37,6 +37,7 @@ export class LoginComponent implements OnInit {
       let dict = res;
       console.log(res);
       if(res.user?.role != 'ADMIN'){
+        //document.getElementById("notFound")?.removeAttribute("hidden");
         this.securityService.notificationAjouter("Vous ne pouvez pas se connecter sur ce site.\nVous devez avoir un accès Admin", "warning");
       }else{
         if(res.user?.is_active == true){
@@ -46,8 +47,10 @@ export class LoginComponent implements OnInit {
           localStorage.setItem('is_superuser', ""+res.user?.is_superuser || "");
           localStorage.setItem('role', ""+res.user?.role || "");
           localStorage.setItem('pageReload', 'true');
-          this.securityService.notificationAjouter("Vous êtes connecté maintenant!", "success");
+          window.location.replace('/dashboard');
+          //this.securityService.notificationAjouter("Vous êtes connecté maintenant!", "success");
         }else{
+
           this.securityService.notificationAjouter(res?.test || "" , "warning");
         }
 
@@ -55,10 +58,11 @@ export class LoginComponent implements OnInit {
         //window.location.reload();
       }
     }, (error) => {
+      document.getElementById("invalid")?.removeAttribute('hidden');
       this.securityService.notificationAjouter("Identifiants incorrects", "danger");
     });
-    
-    
+
+
   }
 
 
